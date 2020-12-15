54b6d5d4207f0900ebaa9d6f8375b38e4cdd2e95
Nathan Fedzen <nfedzen@hotmail.com>
2020-12-15 10:10:42 -0700

    commit tree 934b398197dc0d8b2ec33682932a90c1ec6c9b98

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,22 +2,17 @@ require 'pry'
 
 def get_first_name_of_season_winner(data, season)
   # code here
-  
-  data.each do |key, value|
-    if key == season
-      value.each do |contestant|
-        if contestant["status"] == "Winner"
-          return contestant["name"].partition(" ").first
-        end
-      end
+  data[season].find do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split.first
     end
   end
 end
 
 def get_contestant_name(data, occupation)
   # code here
-  data.each do |key,value|
-    value.each do |contestant|
+  data.each do |current_season, contestants|
+    contestants.find do |contestant|
       if contestant["occupation"] == occupation
         return contestant["name"]
       end
@@ -28,8 +23,8 @@ end
 def count_contestants_by_hometown(data, hometown)
   # code here
   counter = 0
-  data.each do |key, value|
-    value.each do |contestant|
+  data.each do |current_season, contestants|
+    contestants.each do |contestant|
       if contestant["hometown"] == hometown
         counter += 1
       end
@@ -41,7 +36,7 @@ end
 def get_occupation(data, hometown)
   # code here
   data.each do |season, contestants|
-    contestants.each do |contestant|
+    contestants.find do |contestant|
       if contestant["hometown"] == hometown
         return contestant["occupation"]
       end
@@ -51,17 +46,13 @@ end
 
 def get_average_age_for_season(data, season)
   # code here
-  counter = 0
-  ages = 0
-  data.each do |seasons, contestants|
-    if seasons == season
-      contestants.each do |contestant|
-        ages += contestant["age"].to_f
-        counter += 1
-      end
+  total = 0
+  ages = data[season].reduce(0) do |sum_of_ages, contestant|
+        total += 1
+        sum_of_ages += contestant["age"].to_f
     end
-  end
-  
-  average_age= ages / counter
+  average_age = ages / total
   average_age.round
 end
+
+
